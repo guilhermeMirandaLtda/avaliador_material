6c4d30c042212a74c4ba166e5f49aed4614d54c2
guilhermeMirandaLtda <165444589+guilhermeMirandaLtda@users.noreply.github.com>
2024-03-30 00:07:12 -0300

    Update 1_Analisador.py

--- pages/1_Analisador.py
+++ pages/1_Analisador.py
@@ -2,8 +2,20 @@
 import streamlit as st 
 import pandas as pd
 from controllers.rules_handler import RulesHandler
+from io import BytesIO
 
 st.set_page_config(layout="wide")
+
+def to_excel(df):
+    """
+    Função para converter um DataFrame para um arquivo Excel na memória,
+    retornando o conteúdo binário sem chamar .save() explicitamente.
+    """
+    output = BytesIO()
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        df.to_excel(writer, index=False, sheet_name='Sheet1')
+    output.seek(0)
+    return output.getvalue()
 
 # Título do aplicativo
 st.title('Analisador de Aderência de Materiais para Obras')
@@ -12,6 +24,7 @@ st.title('Analisador de Aderência de Materiais para Obras')
 # Upload dos arquivos Excel
 uploaded_file_deposito = st.sidebar.file_uploader("Escolha o arquivo Excel do Depósito", type=['xlsx'])
 uploaded_file_obras = st.sidebar.file_uploader("Escolha o arquivo Excel das Obras", type=['xlsx'])
+
 
 
 # Verificação e leitura dos arquivos
@@ -31,17 +44,20 @@ if uploaded_file_deposito is not None and uploaded_file_obras is not None:
             coverage_df = handler.assess_material_coverage()
             st.write("COBERTURA 100% DOS MATERIAIS")
             coverage_df
+            excel_data = to_excel(coverage_df)
             st.download_button(label='Obras Total',
-                   data=coverage_df,
+                   data=excel_data,
                    file_name='Obras_Total.xlsx',
-                   mime='application/vnd.ms-excel')
+                    mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
 
             # Agora, calcular a cobertura parcial para as obras não totalmente cobertas e atualizar o estoque
             partial_coverage_df = handler.calculate_partial_coverage()
             st.write(" COBERTURA PARCIAL DOS MATERIAIS")
             partial_coverage_df
+            excel_data = to_excel(partial_coverage_df)
+
             st.download_button(label='Obras Parcial',
-                   data=partial_coverage_df,
+                   data=excel_data,
                    file_name='Obras_Parcial.xlsx',
                    mime='application/vnd.ms-excel')
 
@@ -49,8 +65,9 @@ if uploaded_file_deposito is not None and uploaded_file_obras is not None:
             handler.assess_material_coverage()
             st.write("MATERIAL ATENDIDO")
             st.dataframe(handler.df_coverage_analysis)
+            excel_data = to_excel(handler.df_coverage_analysis)
             st.download_button(label='Material',
-                   data=handler.df_coverage_analysis,
+                   data=excel_data,
                    file_name='Material.xlsx',
                    mime='application/vnd.ms-excel')
 
